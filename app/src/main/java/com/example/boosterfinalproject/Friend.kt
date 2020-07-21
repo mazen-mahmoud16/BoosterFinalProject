538a261402073d83622adfd0cd0f60473abfb231
Ingy Mysara <ingymysara@outlook.com>
2020-07-22 00:11:34 +0200

    first commit - 2 designs created but not fully finished

---
+++ app/src/main/java/com/example/boosterfinalproject/Friend.kt
@@ -0,0 +1,11 @@
+package com.example.boosterfinalproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Friend : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friend)
+    }
+}
