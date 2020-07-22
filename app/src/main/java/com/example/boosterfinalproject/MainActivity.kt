1f39588063633d5609715971827e33e315c1c214
Nouran-saad <nouran.asaad@gmail.com>
2020-07-22 06:28:53 +0200

    Screen is completed

--- app/src/main/java/com/example/boosterfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/boosterfinalproject/MainActivity.kt
@@ -2,22 +2,13 @@ package com.example.boosterfinalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_awareness_room.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    private fun getLessonList() : ArrayList<Data> {
-        val LessonList: ArrayList<Data> = ArrayList()
-        LessonList.add(Data("Support System", "Lesson 1", "Building an integral support system,\n" +
-                "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas","Start"))
-        LessonList.add(Data("Support System", "Lesson 2", "Building an integral support system,\n" +
-                "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas","Locked"))
-        LessonList.add(Data("Support System", "Lesson 3", "Building an integral support system,\n" +
-                "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas","Locked"))
-        LessonList.add(Data("Support System", "Lesson 4", "Building an integral support system,\n" +
-                "Pushes you to grow, strech more, mainly asks why things wont work, and bullet proofs ideas","Locked"))
-        return LessonList
+
     }
 }
