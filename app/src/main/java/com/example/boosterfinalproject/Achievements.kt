aa69b5f90fd005797f6534ea6c22ca55c1be2c76
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2020-08-11 13:06:04 +0200

    delete unnecessarily activities

--- app/src/main/java/com/example/boosterfinalproject/Achievements.kt
+++ app/src/main/java/com/example/boosterfinalproject/Achievements.kt
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.activity_achievementss.*
+import kotlinx.android.synthetic.main.fragment_achievements.*
 
 
 class Achievements : Fragment() {
