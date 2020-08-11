04bd197d061d9ca647b2f25ce2b41318bdc9e622
mazen-mahmoud16 <mazenmahmoudabdelhafeez@gmail.com>
2020-08-11 13:33:34 +0200

    Integrate youtube api instead of web view

--- app/src/main/java/com/example/boosterfinalproject/lesson1refrence.kt
+++ app/src/main/java/com/example/boosterfinalproject/lesson1refrence.kt
@@ -1,17 +1,44 @@
 package com.example.boosterfinalproject
 
-import android.annotation.SuppressLint
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
 import kotlinx.android.synthetic.main.activity_lesson1refrence.*
-@SuppressLint("SetJavaScriptEnabled")
-class lesson1refrence : AppCompatActivity() {
+
+class lesson1refrence : YouTubeBaseActivity() {
+
+    companion object {
+        val videoID="a4NT5iBFuZs"
+        val youTubeApiKey="AIzaSyDSBeOuBTMocAONqmU00nF9kgCB94g0LYo"
+    }
+
+    lateinit var youtubePlayerInit : YouTubePlayer.OnInitializedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lesson1refrence)
 
-        videoView.settings.javaScriptEnabled = true
-        videoView.loadUrl("https://www.youtube.com/embed/n7WRDmwqLec")
+        initUI()
+
+    }
+
+    private fun initUI() {
+        youtubePlayerInit = object : YouTubePlayer.OnInitializedListener{
+            override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, youTubePlayer: YouTubePlayer?, p2: Boolean) {
+                youTubePlayer?.loadVideo(videoID)
+            }
+
+            override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+                Toast.makeText(applicationContext, "No available video, Please try again later",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+
+            videoView.initialize(youTubeApiKey,youtubePlayerInit)
 
     }
 }
